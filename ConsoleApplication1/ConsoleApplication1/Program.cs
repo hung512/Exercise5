9db31fd55a7a313a8c07a191aeab1b5839d10c24
minhoo1505 <teamtheweeknd2016@gmail.com>
2019-10-10 09:39:45 +0700

    Exercise05

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,50 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Write(IsUpper(' '));
+            Console.Write(IsUpper1(' '));
+            Console.Write(IsLower(' '));
+            Console.Write(IsLower1(' '));
+        }
+        public static bool IsUpper(char c)
+        {
+            for(int i=65;i<90;i++)
+            {
+                Console.WriteLine(i);
+                if (c.Equals(i))
+                    return true;
+            }
+            return false;
+        }
+        public static bool IsLower(char c)
+        {
+            for(int i=97;i<122;i++)
+            {
+                Console.Write(i);
+                if (c.Equals(i))
+                    return true;
+            }
+            return false;
+        }
+        public static bool IsUpper1(char c)
+        {
+            for(int i=97; i<122;i++)
+            {
+                Console.WriteLine(i);
+                if(c.Equals(i));
+                return false;
+            }
+            return true;
+        }
+        public static bool IsLower1(char c)
+        {
+            for(int i=65;i<90;i++)
+            {
+                Console.WriteLine(i);
+                if (c.Equals(i));
+                return false;
+            }
+            return true;
         }
     }
 }
